e2e3d5f90f8d9a562f1afdae22a068643933fceb
Iain <iain@iainferguson.org>
2020-05-28 22:11:44 +0100

    solved palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    string = string.replace(/\W/g, "");
+    let stringArray = string.toLowerCase().split("");
+    let stringBackwards = [];
+    stringArray.forEach(element => stringBackwards.unshift(element));
+    let newString = stringBackwards.join("").toLowerCase();
+    return newString == string.toLowerCase();
 }
 
 module.exports = palindromes
