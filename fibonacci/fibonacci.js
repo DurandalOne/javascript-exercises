de72d629606377580d91238ffe4afcff5a4e4b6b
Iain <iain@iainferguson.org>
2020-05-28 23:18:26 +0100

    completed fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 1) {return "OOPS"}; 
+    let numArray = [1, 1]
+    for (i = 1; i < a; i++) {
+        numArray.push(numArray[numArray.length - 2] + numArray[numArray.length - 1]);
+    }
+    return numArray[a - 1];
 }
 
 module.exports = fibonacci
