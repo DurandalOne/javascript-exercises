a99cfdaf72f36328e5d6096d39e2f4446d1af4dc
Iain <iain@iainferguson.org>
2020-05-29 23:53:07 +0100

    solved findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+    people.forEach(e => {
+        if (typeof(e.yearOfDeath) === "undefined") {
+            e.age = (new Date().getFullYear()) - e.yearOfBirth;
+        } else {
+            e.age = e.yearOfDeath - e.yearOfBirth;
+        }
+    });
 
+    let max = people[0].age
+    let theOldest = [];
+
+    for (i = 0; i < people.length; i++) {
+        let v = people[i].age;
+        max = (v > max) ? v : max;
+    }
+    for (var i = 0; i < people.length; i++) {
+        if (people[i].age === max) {
+            theOldest =  people[i];
+        };
+    } 
+    return theOldest;
 }
 
 module.exports = findTheOldest
